519d235a242a1c3084a26d98e1994af3ec71c07c
LoriMinor <lorim808@gmail.com>
2015-02-06 19:05:53 -0500

    initial commit

---
+++ StudentPortfolioService/StudentPortfolioService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentPortfolioService.Startup))]
+namespace StudentPortfolioService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
